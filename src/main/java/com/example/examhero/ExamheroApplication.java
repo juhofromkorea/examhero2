a98bd5ea23d1995f3455296412b7a1aee0579d60
riparang <jiyuho.kimu@grp.tmnf.jp>
2026-06-02 10:20:58 +0900

    #0 server setting

---
+++ src/main/java/com/example/examhero/ExamheroApplication.java
@@ -0,0 +1,13 @@
+package com.example.examhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamheroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamheroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/examhero/ExamheroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamheroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
