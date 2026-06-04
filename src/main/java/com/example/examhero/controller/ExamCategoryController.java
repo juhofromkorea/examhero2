c7798e172dd2fb29ba6664f4511a81932e074c8c
yt612 <TOMOHIRO.YAMADA6@grp.tmnf.jp>
2026-06-04 16:26:37 +0900

    ExamCategoryController

--- src/main/java/com/example/examhero/controller/ExamCategoryController.java
+++ src/main/java/com/example/examhero/controller/ExamCategoryController.java
@@ -3,14 +3,24 @@ package com.example.examhero.controller;
 import java.security.Principal;
 import java.util.List;
 
+import jakarta.validation.Valid;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.examhero.dto.ExamCategoryForm;
 import com.example.examhero.entity.ExamCategory;
 import com.example.examhero.entity.User;
 import com.example.examhero.service.ExamCategoryService;
+
+
 
 @Controller
 public class ExamCategoryController {
@@ -21,20 +31,55 @@ public class ExamCategoryController {
         this.examCategoryService = examCategoryService;
     }
 
-    @GetMapping("/ExamCategory")
-    public String ExamCategory(Model model, Principal principal){
+    @GetMapping("/category")
+    public String ExamCategory( @AuthenticationPrincipal UserDetails userDetails,
+            Model model, Principal principal){
 
         String email = principal.getName();
         User user = examCategoryService.findUserByEmail(email);
 
-        // ② そのお客さんの「カテゴリ一覧」をシェフにもらって、お盆に乗せる
         List<ExamCategory> categories = examCategoryService.findCategoriesByUser(user);
         model.addAttribute("categories", categories);
 
-        // ③ 新しいカテゴリを作るための「真っ白な注文票」をお盆に乗せる
         model.addAttribute("examCategoryForm", new ExamCategoryForm());
     
 
-        return "ExamCategory";
+        return "category/list";
     }
+
+    @GetMapping("/category/new")
+    public String newform(Model model) {
+
+    model.addAttribute("examCategoryForm", new ExamCategoryForm());
+
+    return "categories/form";
+    }
+
+    @PostMapping("/category")
+    public String create(
+        @Valid @ModelAttribute("examCategoryForm") ExamCategoryForm form,
+        BindingResult bindingResult,
+        @AuthenticationPrincipal UserDetails userDetails,
+        RedirectAttributes redirectAttributes){
+
+        if(bindingResult.hasErrors()){
+            return "category/form";
+        }
+
+        try{
+            User loginUser = examCategoryService.findUserByEmail(userDetails.getUsername());
+
+            examCategoryService.createCategory(form, loginUser);    
+            redirectAttributes.addFlashAttribute("successMessage", "カテゴリを作成しました");
+            return "redirect:/vategory";
+
+    } catch (IllegalArgumentException e){
+         bindingResult.reject("categoryError", e.getMessage());
+            return "categories/form";
+        }
+    }
+    
+
+
+
 }
